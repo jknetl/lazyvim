5ba8574f08ee7c5e7e84cdcd1bf937fd38b5c98f
Jakub Knetl <knetl.j@gmail.com>
2026-09-04 15:31:25 +0200

    Override default clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LazyVim defaults clipboard to "unnamedplus", syncing every yank/delete with
+-- the system clipboard. Revert to Neovim's default (only explicit "+/"* yanks).
+vim.opt.clipboard = ""
