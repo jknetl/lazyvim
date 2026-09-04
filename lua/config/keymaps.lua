2f9e8c2ee0ff027764ef8c7afe864c3461801d7e
Jakub Knetl <knetl.j@gmail.com>
2026-09-04 15:51:52 +0200

    Allow to start picker from current buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Find Files, rooted at the current buffer's directory
+vim.keymap.set("n", "<leader>fo", function()
+  LazyVim.pick("files", { cwd = vim.fn.expand("%:p:h") })()
+end, { desc = "Find Files (Buffer Dir)" })
